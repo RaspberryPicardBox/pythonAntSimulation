e147fe4a2f41c6530bb2f807002f91c6d2707f2c
RaspberryPicardBox <raspberrypicardbox@gmail.com>
2022-04-29 20:10:48 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+---An ant colony simulation written solely in Python---
+
+Pixel "ants" will spread out from the colony home, trying to find food.
+Ants will return home following pheromone paths when food has been found.
+
+object types for now: air, ant, pheromone, food, home
+
+By Michael B
+"""
+
+if __name__ == '__main__':
+    print("Hello World")
